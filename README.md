40a07d0dc2419c338492042549cb28a39fe69683
Nhut Do <ext.Nhut.Do@rovio.com>
2021-05-25 16:15:44 +0300

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# LoginWithAmazon
-Basic Log in with Amazon
+# Login With Amazon example
+Basic Log in with Amazon example.
+
+Fill in your Amazon "Client ID" in index.html-file under src/resources/static-folder.
+
+Start application on Windows:
+mvnw.cmd spring-boot:run
+
+(TODO: Add HTTPS)<br/>
+To access the page: http://localhost:8080/index.html
---
+++ src/main/java/fi/donhut/lwa/LogInWithAmazonApplication.java
@@ -0,0 +1,13 @@
+package fi.donhut.lwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogInWithAmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogInWithAmazonApplication.class, args);
+	}
+
+}
